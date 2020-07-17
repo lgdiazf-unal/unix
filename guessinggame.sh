94d7cf453cd20d4de080fb9f333d13c339b0838f
Luis <lgdiazf@unal.edu.co>
2020-07-17 02:42:29 +0000

    v1

---
+++ guessinggame.sh
@@ -0,0 +1,20 @@
+function getNumber {
+    number=$(ls | wc -l)
+}
+getNumber
+echo "guess the number of files in the current directory:"
+read response
+echo $number 
+while [[ $response -ne $number ]]
+do
+    if [[ $response -gt $number ]]
+    then
+        echo "$response is too high"
+        echo "guess the number of files in the current directory:"
+    else
+        echo "$response is too low"
+        echo "guess the number of files in the current directory:"
+    fi
+    read response
+done
+echo "----- good work!!!! ------"
